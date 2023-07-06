0b2891afdb9cb3a378864bc730dcd0d586d24623
Sameer Rifat <sameerrifatbrw789@gmail.com>
2023-07-06 12:02:30 +0500

    added sameSite none

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,6 +8,8 @@ const sendToken = (user, statusCode, res)=>{
             Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000
         ),
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
         // domain: ".netlify.app" // Set the common parent domain here
     }
     res.status(statusCode).cookie("token", token, options).json({
