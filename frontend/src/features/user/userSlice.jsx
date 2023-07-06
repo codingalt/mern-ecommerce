070af99394a3a0623799e2a3e5a529df1e24edca
Sameer Rifat <sameerrifatbrw789@gmail.com>
2023-07-06 16:23:10 +0500

    changes

--- frontend/src/features/user/userSlice.jsx
+++ frontend/src/features/user/userSlice.jsx
@@ -9,6 +9,7 @@ const initialState = {
 }
 // login
 export const login = createAsyncThunk('user/login', async (data) => {
+    console.log("login api called")
     const config = { headers: { 'Content-Type': 'application/json' }, withCredentials: true };
     try {
         const response = await axios.post(
