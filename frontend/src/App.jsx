b4ce53249911f208605281f3d2044943e704b0dc
Sameer Rifat <sameerrifatbrw789@gmail.com>
2023-07-06 14:04:46 +0500

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -58,6 +58,7 @@ const App = () => {
     store.dispatch(loadUser());
     getStripeApiKey()
   }, [])
+  console.log(stripeApiKey)
   return (
     <>
       {/* {showHeaderAndFooter && <Navbar />} */}
