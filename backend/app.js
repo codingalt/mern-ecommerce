c0d2b23d4e40f3c9cd11ac4accd7a5024d2cc05b
codingalt <106727907+codingalt@users.noreply.github.com>
2023-07-20 21:52:47 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-// const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-// const path = require('path')
+const path = require('path')
 
 // config
 if(process.env.NODE_ENV !== 'PRODUCTION'){
@@ -13,14 +13,19 @@ if(process.env.NODE_ENV !== 'PRODUCTION'){
 
 const errorMiddleware = require("./middleware/error")
 
-const corsOptions = {
+// const corsOptions = {
+//   origin: true,
+//   credentials: true,
+// };
+
+app.use("*", cors({
   origin: true,
-  credentials: true,
-};
+  credentials: true
+}));
 
 app.use(express.json());
-// app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cookieParser());
+// app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
